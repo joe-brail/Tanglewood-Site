acee20d5411d948e7620c7f38b3152a68f0b6ec7
joe-brail <joebrailsford492@gmail.com>
2021-05-28 13:50:43 +1000

    commit

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
-const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const menu = document.querySelector('.menu_content');
-    const menu_items = document.querySelectorAll('.menu_content a');
+// const navSlide = () => {
+//     const burger = document.querySelector('.burger');
+//     const menu = document.querySelector('.menu_content');
+//     const menu_items = document.querySelectorAll('.menu_content a');
 
-    burger.addEventListener('click', () => {
+//     burger.addEventListener('click', () => {
 
 
-        menu.classList.toggle('menu_active');
+//         menu.classList.toggle('menu_active');
 
-        // menu_items.forEach((link, index) => {
-        //     if (link.style.animation) {
-        //         link.style.animation = '';
-        //     }   else {
-        //         link.style.animation = 'menu_itemsFade 0.5s ease forwards ${index / 8 + 0.3}s';
-        //     }
-        // )};
+//         // menu_items.forEach((link, index) => {
+//         //     if (link.style.animation) {
+//         //         link.style.animation = '';
+//         //     }   else {
+//         //         link.style.animation = 'menu_itemsFade 0.5s ease forwards ${index / 8 + 0.3}s';
+//         //     }
+//         // )};
 
-        //Burger animation
-        burger.classList.toggle ('toggle');
+//         //Burger animation
+//         burger.classList.toggle ('toggle');
 
-    });
+//     });
 
     
 
-}
+// }
 
-navSlide ();
+// navSlide ();
